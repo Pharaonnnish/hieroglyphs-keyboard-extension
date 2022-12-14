a964de599f695bb2ce420bbafb9d6a00ae2efb4b
Rami <ramiabrahem24@gmail.com>
2022-12-14 18:11:54 +0200

    add keyboard container

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,26 @@
-<div>Initial Commit</div>
+<div class="hieroglyphs-keyboard">
+  <div class="title-bar"><b>Hieroglyphs Keyboard</b></div>
+</div>
+
+<style>
+  .hieroglyphs-keyboard {
+    position: absolute;
+    bottom: 4px;
+    right: 4px;
+    width: 45vw;
+    height: 280px;
+    overflow: hidden;
+    border-radius: 4px;
+    box-shadow: hsla(0, 0%, 0%, 0.24) 0px 3px 8px;
+  }
+
+  .title-bar {
+    text-align: center;
+    padding: 6px 4px;
+    font-family: sans-serif;
+    font-size: 13px;
+    background: hsla(37, 98%, 53%, 0.4);
+    user-select: none;
+    cursor: move;
+  }
+</style>
